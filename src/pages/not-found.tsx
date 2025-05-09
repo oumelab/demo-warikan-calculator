c93b878208e20c62ed13bf505353c85aa5066a07
oume-mini <oumelab@pt.rdy.jp>
2025-05-09 20:02:40 +0900

    not-foundページを修正

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router";
+
 export default function NotFound() {
   return (
     <div className="py-24">
       <h1 className="text-4xl font-bold text-center">404 Not Found</h1>
+      <p className="text-center">ページが見つかりませんでした。</p>
+      <p className="text-center mt-8"><Link to="/" className="underline underline-offset-2">トップページに戻る</Link></p>
     </div>
   )
 }
